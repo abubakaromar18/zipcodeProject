34dfb4ba777bb402208ed650ce8dfa87a5b4ed62
abubakaromar18 <37700067+abubakaromar18@users.noreply.github.com>
2021-02-24 17:55:49 -0600

    This is a practice code to see if github is connected to visual studio

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    cout << "Hello, what is you name: ";
+    cin >> name;
+    cout << "How old are you: ";
+    cin >> age;
+
+    cout << "Your name is " << name << " and you are " << age << " years old"; 
+    return 0;
+}
