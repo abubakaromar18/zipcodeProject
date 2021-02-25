8acffa4622bde9d7fbea6f2cc00fc87cbcb866f0
abubakaromar18 <37700067+abubakaromar18@users.noreply.github.com>
2021-02-24 18:32:38 -0600

    fresh start

--- Main.cpp
+++ Main.cpp
@@ -4,13 +4,5 @@ using namespace std;
 
 int main()
 {
-    string name;
-    int age;
-    cout << "Hello, what is you name: ";
-    cin >> name;
-    cout << "How old are you: ";
-    cin >> age;
-
-    cout << "Your name is " << name << " and you are " << age << " years old"; 
-    return 0;
+    
 }
